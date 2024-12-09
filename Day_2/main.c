225ee5ee72a67ce3829b69d5b276bb6ea3172d9c
Luna <cotact@aki-dev.com>
2024-12-09 01:28:12 +0100

    Working on day 2, linked list

--- Day_2/main.c
+++ Day_2/main.c
@@ -2,6 +2,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <memory.h>
+
+typedef struct Node {
+  struct Node* next;
+  int data;
+} node_t;
+
+node_t* new_linked_list(int headData)
+{
+  node_t* head = NULL;
+  head = (node_t*)malloc(sizeof(node_t));
+  if (head == NULL) return head;
+  head->data = headData;
+  head->next = NULL;
+  return head;
+}
+
+node_t* push(node_t* relativeHead, int nodeData)
+{
+  node_t* node = NULL;
+  node = (node_t*)malloc(sizeof(node_t));
+  if (node == NULL) return node;
+  
+  relativeHead->next = node;
+  node->data = nodeData;
+  node->next = NULL;
+  return node;
+}
+
+int pop(node_t** head)
+{
+  int retval = -1;
+  node_t* next_node = NULL;
+  if (*head == NULL) return -1;
+  
+  next_node= (*head)->next;
+  retval = (*head)->data;
+  free(*head);
+  *head = next_node;
+
+  return retval;  
+}
 
 int main(void)
 {
@@ -14,17 +56,14 @@ int main(void)
   // Read file into buffer
   inputFp = fopen("../aoc-2.txt", "r");
 
-  
-
   for (;;) {
     int ch = fgetc(inputFp);
     sprintf(currLine,"%c", ch);
     printf("Current report: %s\n", currLine);
    
-    
     i++;
     if (ch == '\n') {
-      int firstNum = currLine[0] // TODO
+      int firstNum = currLine[0];
       i = 0; 
       memset(currLine, ' ', 26); 
     } 
